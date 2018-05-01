32fe1f9df2c0f1f34375579019ab73460b4ad5f3
Leonardo Graterol <pankas87@gmail.com>
2018-04-30 23:22:10 -0300

    Logging: -index.js -> Server initialization -controllers/users/listUsers -> Actions and errors

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,14 +1,20 @@
 import { OK, INTERNAL_SERVER_ERROR } from 'http-status';
+import Logger from '../helpers/logger';
 import { fetchUsers } from '../services/users';
 
 const listUsers = function ListUsers(req, res, next) {
+    const logger = new Logger(req.headers);
+
+    logger.info('action=listUsers description=begin');
     const usersPromise = fetchUsers();
 
     usersPromise.then((users) => {
         res.status(OK);
         res.send(users);
+        logger.info(`action=listUsers description=success message="${users.length} users found"`);
     }).catch((error) => {
         res.sendStatus(INTERNAL_SERVER_ERROR);
+        logger.error(`action=listUsers description=error message=${error.message}`);
     });
     return next();
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import Logger from './helpers/logger';
 import app from './app';
 
 const port = process.env.NODE_PORT || 8080;
+const logger = new Logger();
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    logger.info(`Listening on port ${port}`);
 });
